be17f24d517c1cfe2b99906e77b6ccc1bda04c47
Şafak Öksüzer <safak.oksuzer@getir.com>
2023-02-05 10:45:30 +0300

    [Fix] Remove importing package.json

--- src/config/swagger.js
+++ src/config/swagger.js
@@ -1,15 +1,8 @@
-import { createRequire } from 'node:module';
-import Config from './index';
-
-const require = createRequire(import.meta.url);
-
-const Package = require('../../package.json');
-
 const swaggerDef = {
   openapi: '3.0.0',
   info: {
     title: 'Ethereum Balance Example',
-    version: Package.version,
+    version: 'v1.0.0',
     license: {
       name: 'MIT',
       url: 'https://github.com/safakoks/ethereum-balance-example/blob/master/LICENSE',
